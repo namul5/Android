5d186bf2135f915113b687e95502d8ab6650bae0
Luke Lim <solnamul@gmail.com>
2017-06-14 21:07:25 +0900

    ..

--- DemoApp/app/src/main/java/com/gamearena/demoapp/MainActivity.java
+++ DemoApp/app/src/main/java/com/gamearena/demoapp/MainActivity.java
@@ -4,15 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
-private FirebaseAnalytics mFirebaseAnalytics;
+//private FirebaseAnalytics mFirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Obtain the FirebaseAnalytics instance.
-        FirebaseAnalytics mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        //FirebaseAnalytics mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //IgawCommon.autoSessionTracking(com.gamearena.demoapp.MainActivity.this);
     }
 }
